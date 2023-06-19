98349e03a8f72d932d97ae40d2d8575ee5b014ea
Jonathan PAIN-CHAMMING'S <jonathan.pchs@gmail.com>
2023-06-19 16:48:09 +0200

    Lesson 13 - Ajout de la function de recherche

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 /**
  * Pour le moment ceci est notre vue du composant Home.
  * Il construit la vue globale intégrant la barre de recherche et l'affichage de son résultat.
+ * Il inclut également une partie du controleur.
  */
 
 import { Component, inject } from '@angular/core';
@@ -16,24 +17,36 @@ import { HousingService } from '../housing.service';
   template: `
   <section>
     <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
+      <input type="text" placeholder="Filter by city" #filter>
+      <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
     </form>
   </section>
   <section class="results">
     <app-housing-location
-      *ngFor="let housingLocation of housingLocationList"
+      *ngFor="let housingLocation of filteredLocationList"
       [housingLocation]="housingLocation">
     </app-housing-location>
   </section>
 `,
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+    console.log(this.filteredLocationList);
   }
 }
