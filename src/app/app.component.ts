c2a32a353d6307543bd861ab7cb94ff69f8640bd
Jonathan PAIN-CHAMMING'S <jonathan.pchs@gmail.com>
2023-06-19 11:32:34 +0200

    Lesson 10 - Mise en place du routeur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,27 @@
 /**
- * Ceci est le point d'entrée de notre application, le main de cette dernière
+ * Ceci est le point d'entrée de notre application,
+ * c'est ici que l'on appelle le routeur ainsi que le template.
  */
 
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
-  template: `  
+  imports: [HomeComponent, RouterModule],
+  template: `
   <main>
-  <header class="brand-name">
-    <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-  </header>
-  <section class="content">
-    <app-home></app-home>
-  </section>
-</main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
 `,
   styleUrls: ['./app.component.css'],
 })
